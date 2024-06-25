7620888a92bf693be831d0fbd5acfd2477ccee84
hassan anwar <anwar1123@hotmail.co.uk>
2024-06-25 15:46:28 +0100

    Adding a unit test to see if the format is okay before creating the service.

--- HolidaySearch.Tests/UnitTest1.cs
+++ HolidaySearch.Tests/UnitTest1.cs
@@ -1,10 +1,20 @@
-namespace HolidaySearch.Tests;
+using HolidaySearch.Models;
+using System;
+using System.IO;
+using System.Linq;
+using Xunit;
 
-public class UnitTest1
+namespace HolidaySearch.Tests
 {
-    [Fact]
-    public void Test1()
+    public class HolidaySearchServiceTests
     {
+        private readonly HolidaySearchService _holidaySearchService;
 
+        public HolidaySearchServiceTests()
+        {
+            string flightsJsonPath = Path.Combine(AppContext.BaseDirectory, "flights.json");
+            string hotelsJsonPath = Path.Combine(AppContext.BaseDirectory, "hotels.json");
+            _holidaySearchService = new HolidaySearchService(flightsJsonPath, hotelsJsonPath);
+        }
     }
 }
